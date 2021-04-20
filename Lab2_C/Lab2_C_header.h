bffba0c44e5baf6d4fe411861437c9161dacbee9
Gromiko-Evgeniy <74821242+Gromiko-Evgeniy@users.noreply.github.com>
2021-04-20 14:24:51 +0300

    Add files via upload

---
+++ Lab2_C/Lab2_C_header.h
@@ -0,0 +1,4 @@
+float SMSprice(float _SMScount);
+float TheSameNetworkCallPrice(float _talkTime_toTheSameNetwork);
+float OtherNetworkCallPrice(float _talkTime_toOtherNetwork);
+float UrbanNetworkCallPrice(float _talkTime_toUrbanNetwork);
